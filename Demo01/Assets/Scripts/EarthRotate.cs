99b1584798c79acc929c9f7014e2c9b1f1447d6d
Rourss <1521093584@qq.com>
2019-08-05 18:09:27 +0800

    增加脚本，完成自转和绕太阳转

---
+++ Demo01/Assets/Scripts/EarthRotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthRotate : MonoBehaviour {
+
+    public Transform sun;
+	// Use this for initialization
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(sun.position, Vector3.up, 2f);
+        transform.Rotate(Vector3.up, 1f);
+    }
+}
