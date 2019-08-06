48ef707d11a39abbba17ebe5660f28175cc2d3be
ROURSS <47970410+Rourss@users.noreply.github.com>
2019-08-06 17:15:30 +0800

    通过脚本控制第三人称的主摄像机

--- Demo03/Assets/Scripts/CubeMove.cs
+++ Demo03/Assets/Scripts/CubeMove.cs
@@ -5,11 +5,13 @@ public class CubeMove : MonoBehaviour
 {
     private float speed;
     private float anglSpeed;
+    private Camera ThirdCamera;
     // Use this for initialization
     void Start()
     {
         speed = 50f;
         anglSpeed = 10f;
+        ThirdCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -17,5 +19,10 @@ public class CubeMove : MonoBehaviour
     {
         transform.Rotate(Vector3.up * speed * Time.deltaTime * Input.GetAxisRaw("Horizontal"));
         transform.Translate(Vector3.forward * anglSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical"));
+    }
+    void LateUpdate()
+    {
+        //通过脚本让主摄像机跟着物体移动，但不改变跟着物体改变视角
+        ThirdCamera.transform.position = transform.position + new Vector3(0, 1.96f, -14.45f);
     }
 }
