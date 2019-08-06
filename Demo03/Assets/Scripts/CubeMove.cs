dd4adfafd2d5d419e1bde0f4cd0db0e091342c7c
ROURSS <47970410+Rourss@users.noreply.github.com>
2019-08-06 17:03:09 +0800

    第一人称和第三人称视角（作为物体的子对象实现的）

---
+++ Demo03/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMove : MonoBehaviour
+{
+    private float speed;
+    private float anglSpeed;
+    // Use this for initialization
+    void Start()
+    {
+        speed = 50f;
+        anglSpeed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime * Input.GetAxisRaw("Horizontal"));
+        transform.Translate(Vector3.forward * anglSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical"));
+    }
+}
