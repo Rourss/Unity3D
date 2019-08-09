69bd5b70a80ef1bc11c492619d2c472c61ff1214
ROURSS <47970410+Rourss@users.noreply.github.com>
2019-08-09 12:12:15 +0800

    孙悟空Demo

---
+++ Demo07/Assets/Scripts/CCMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CCMove : MonoBehaviour
+{
+
+    private CharacterController CC;
+    private Animator WuKong;
+    private float speed;    
+    // Use this for initialization
+    void Start()
+    {
+        CC = GetComponent<CharacterController>();
+        WuKong = GetComponent<Animator>();
+        speed = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            WuKong.SetBool("IsWalk", true);
+            CC.SimpleMove(transform.forward * speed * Time.deltaTime * Input.GetAxisRaw("Horizontal"));
+
+        }
+        if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            WuKong.SetBool("IsWalk", false);
+        }
+
+    }
+}
