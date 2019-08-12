8b7bfd26254ca064f43043576a714ceebb781b14
rourss <1521093584@qq.com>
2019-08-12 18:15:31 +0800

    在地图Demo上增加人物

---
+++ Demo07/Assets/Scripts/MobileMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileMove : MonoBehaviour
+{
+
+    private float speed;
+    private float angSpeed;
+    // Use this for initialization
+    void Start()
+    {
+        speed = 2f;
+        angSpeed = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * Input.GetAxisRaw("Vertical"));
+        transform.Rotate(Vector3.up * angSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal"));
+    }
+}
---
+++ Demo07/Assets/Scripts/ThirdCameraMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdCameraMove : MonoBehaviour
+{
+
+    public GameObject game;
+    private Vector3 ver;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void LateUpdate()
+    {
+        ver = game.transform.position;
+        this.transform.localPosition = Vector3.Lerp(this.transform.position, ver + new Vector3(0, 3f, -4f), Time.deltaTime);
+        
+    }
+}
