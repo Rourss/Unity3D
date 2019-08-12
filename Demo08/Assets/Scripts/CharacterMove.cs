56453f6ba4646505edb72357d0b1e527fa0f0963
rourss <1521093584@qq.com>
2019-08-12 17:13:10 +0800

    人物Demo

---
+++ Demo08/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour {
+
+    private float speed;
+    private float angspeed;
+    private float ver;
+    private float hor;
+    private Animator ObjGame;
+	// Use this for initialization
+	void Start () {
+        speed = 10f;
+        angspeed = 100f;
+        ObjGame = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void FixedUpdate()
+    {
+        ver = Input.GetAxisRaw("Vertical");
+        hor = Input.GetAxisRaw("Horizontal");
+        if (ver > 0)
+        {
+            ObjGame.SetBool("IsWalk", true);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                ObjGame.SetBool("IsRun", true);
+            }
+            else
+            {
+                ObjGame.SetBool("IsRun", false);
+            }
+        }
+        else if (ver < 0)
+        {
+            ObjGame.SetBool("IsWalkBack", true);
+            ObjGame.SetBool("IsRun", false);
+        }
+        else
+        {
+            ObjGame.SetBool("IsWalk", false);
+            ObjGame.SetBool("IsWalkBack", false);
+            ObjGame.SetBool("IsRun", false);
+        }
+
+        if (hor > 0)
+        {
+            ObjGame.SetBool("IsWalkR", true);
+        }
+        else if (hor < 0)
+        {
+            ObjGame.SetBool("IsWalkL", true);
+        }
+        else
+        {
+            ObjGame.SetBool("IsWalkR", false);
+            ObjGame.SetBool("IsWalkL", false);
+        }
+        
+    }
+}
---
+++ Demo08/Assets/Scripts/ThirdCameraMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdCameraMove : MonoBehaviour
+{
+
+    public GameObject game;
+    private Vector3 ver;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ver = new Vector3();
+        ver = game.transform.position;
+    }
+    void LateUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, ver + new Vector3(0, 2f, -5f), Time.deltaTime);
+    }
+}
