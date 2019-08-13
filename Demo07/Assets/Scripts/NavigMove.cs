a04e277d666e0fdbdce1f9ba19a14334f25e6c76
rourss <1521093584@qq.com>
2019-08-13 18:13:36 +0800

    优化+新地图Demo

---
+++ Demo07/Assets/Scripts/NavigMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigMove : MonoBehaviour
+{
+
+    private NavMeshAgent nav;
+    // Use this for initialization
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit, 100))
+            {
+                nav.SetDestination(hit.point);
+            }
+        }
+    }
+}
