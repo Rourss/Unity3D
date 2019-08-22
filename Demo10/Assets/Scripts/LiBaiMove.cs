b2c8c9a774fce2229c83df7324229d677ea9669f
rourss <1521093584@qq.com>
2019-08-22 18:04:18 +0800

    加入动画

--- Demo10/Assets/Scripts/LiBaiMove.cs
+++ Demo10/Assets/Scripts/LiBaiMove.cs
@@ -5,27 +5,69 @@ public class LiBaiMove : MonoBehaviour
 {
     private Animator libai;
     private bool isTrigger;
+    private bool stop;
+    private bool start;
+    private float speed;
+    private int vec;
+    private float vecAims=150;
     // Use this for initialization
     void Start()
     {
+        speed =1f;
         isTrigger = true;
+        stop = true;
+        start = false;
+        vecAims = -2f;
         libai = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        YouShen();
+        Zoulu();
+        Zuozhuan();
+
+
+    }
+    void YouShen()
+    {
         if (isTrigger)
         {
             //Youzhuan();
-            transform.Rotate(Vector3.down * 70f );
             libai.SetBool("youzhuan",true);
+            transform.Rotate(Vector3.down * 90f);
+            //transform.Rotate(0, - (num* Time.deltaTime *20f), 0);
+            isTrigger = false;
         }
-        isTrigger = false;
-
+        
     }
-    void Youzhuan()
+    void Zoulu()
     {
-        transform.RotateAround(transform.position, new Vector3(0, -90, 0), Time.deltaTime * 100f);
+        if (stop)
+        {
+            vec = Mathf.RoundToInt(transform.position.x);
+            if (vec <= -3)
+            {
+                stop = false;
+                libai.SetBool("youzhuan", false);
+            }
+            else
+            {
+                libai.SetFloat("zoulu", speed);
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+        }
+    }
+    void Zuozhuan()
+    {
+        if (start == false && stop == false)
+        {
+            libai.SetBool("zuozhuan", true);
+            transform.Rotate(Vector3.down * 90f);
+            libai.SetBool("zuozhuan", false);
+            start = true;
+        }
+        
     }
 }
