2143e20880a16123ef263adbe8f9321f6b639bc9
rourss <1521093584@qq.com>
2019-08-23 15:50:25 +0800

    调试

--- Demo10/Assets/Scripts/RingMove.cs
+++ Demo10/Assets/Scripts/RingMove.cs
@@ -2,37 +2,63 @@
 using System.Collections;
 using UnityEngine.UI;
 using VRStandardAssets.Utils;
+using System;
 
 public class RingMove : MonoBehaviour
 {
     private  Image ring;
-    private bool OnTrigger = false;
+    private bool onTrigger = false;
     private VRInteractiveItem activ;
+    public event Action publicOnOver;
+    public event Action publicOnOut;
+    public event Action publicLoad;
     void Awake()
     {
         ring = GameObject.Find("ring").GetComponent<Image>();
         activ = gameObject.GetComponent<VRInteractiveItem>() ?? gameObject.AddComponent<VRInteractiveItem>();
         ring.fillAmount = 0;
     }
+    void OnEnable()
+    {
+        activ.OnOver += OnOver;
+        activ.OnOut += OnOut;
+    }
+    void OnDisble()
+    {
+        activ.OnOver -= OnOver;
+        activ.OnOut -= OnOut;
+    }
     void OnOver()
     {
-        OnTrigger = true;
+        onTrigger = true;
+        if(publicOnOver!=null)
+        {
+            publicOnOver();
+        }
     }
     void OnOut()
     {
-        OnTrigger = false;
+        onTrigger = false;
         ring.fillAmount = 0;
+        if (publicOnOut != null)
+        {
+            publicOnOut();
+        }
     }
-    void  Fill()
+   public void  Fill()
     {
-        //if (OnTrigger)
-        //{
+        if (onTrigger)
+        {
             ring.fillAmount += Time.deltaTime / 2;
-        //}
-        
+        }
+
         if (ring.fillAmount == 1)
         {
             ring.fillAmount = 0;
+        }
+        if (publicLoad != null)
+        {
+            publicLoad();
         }
     }
     void Update()
---
+++ Demo10/Assets/Scripts/VRButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VRButton : MonoBehaviour
+{
+    public Sprite overImage;
+    public Sprite outImage;
+    private RingMove publicRing;
+    public GameObject ui;
+    void Awake()
+    {
+        publicRing = gameObject.GetComponent<RingMove>()?? gameObject.AddComponent<RingMove>();
+    }
+    void OnEnable()
+    {
+        publicRing.publicOnOver += OnOver;
+        publicRing.publicLoad += Load;
+        publicRing.publicOnOut += OnOut;
+    }
+    void OnDisable()
+    {
+        publicRing.publicOnOver -= OnOver;
+        publicRing.publicLoad -= Load;
+        publicRing.publicOnOut -= OnOut;
+    }
+    void OnOver()
+    {
+        GetComponent<Image>().sprite = overImage;
+    }
+    void OnOut()
+    {
+        GetComponent<Image>().sprite = outImage;
+    }
+    void Load()
+    {
+        if(publicRing)
+        Destroy(ui, 0.1f);
+    }
+}
